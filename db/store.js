a4692ba0c20bd1ca069464b6c29595182e93e4a2
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-06-29 20:24:44 -0400

    added ID to the POST funtion

--- db/store.js
+++ db/store.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const path = require("path");
+const { v1: uuidv1 } = require('uuid');
 
 
 // create POST function
 
-function addNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
+function addNote(note, notesArray) {
+    const { title, text } = note;
+    const newNote = { title, text, id: uuidv1() }
+    notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, "./db.json"),
         JSON.stringify({ notes: notesArray }, null, 2)
